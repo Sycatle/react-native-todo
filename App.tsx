896cf1423a861cb97764c82881725e4a3a91dedb
Sycatle <sycatle@pm.me>
2025-06-19 23:57:53 +0200

    update homepage

--- App.tsx
+++ App.tsx
@@ -1,27 +1,13 @@
-// import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import './global.css';
+import { Text, View } from 'react-native';
 
-// import './global.css';
-
-// export default function App() {
-//   return (
-//     <>
-//       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-//       <StatusBar style="auto" />
-//     </>
-//   );
-// }
-
-import "./global.css"
-import { Text, View } from "react-native";
- 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
+      <Text className="text-3xl font-bold text-black">Welcome to Todo.</Text>
+      <Text className="text-sm text-gray-600">
+        by Sycatle
       </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
